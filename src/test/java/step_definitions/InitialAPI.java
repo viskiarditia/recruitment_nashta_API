cc74d5f84d3e6b7faf9d35b9e55719c3c363c778
viskiarditia <viskiarditia@gmail.com>
2024-01-24 16:57:32 +0700

    Test Case Sign In

--- src/test/java/step_definitions/InitialAPI.java
+++ src/test/java/step_definitions/InitialAPI.java
@@ -1,7 +1,9 @@
 package step_definitions;
 
 public class InitialAPI {
-    public static final String URL = "";
+    public static final String URL = "http://10.10.3.132/rcrt/"; // all testing
+//    public static final String URL = "http://10.10.3.132/v3/sysparam/"; // khusus forgot password
+
     public static final String DIR = System.getProperty("user.dir");
 
     public static final String JSON_SCHEMA_VALIDATOR = DIR + "";
