2439b14dd8c68d68d79e86fd2b5cd6c7ba588c43
viskiarditia <viskiarditia@gmail.com>
2024-01-26 16:15:30 +0700

    clean code 1

--- src/test/java/step_definitions/cucumberTestSuite.java
+++ src/test/java/step_definitions/cucumberTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        tags = "@Login",
+        tags = "@recruitment",
         features = {"classpath:features/"},
         glue = { "classpath:step_definitions"},
         plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
